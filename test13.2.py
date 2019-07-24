33f36979fd96eb1a9028a0c66a5ed6d7182153f5
nishparsekar <nishparsekar@gmail.com>
2019-07-24 17:00:10 +0100

    Added py codes

---
+++ test13.2.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO     
+import time
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)    
+GPIO.setup(7,GPIO.OUT)      
+
+GPIO.output(7,0)            
+time.sleep(.5)               
+                             
+GPIO.output(7,0)           
+time.sleep(.5)
+
+GPIO.cleanup()    
+
